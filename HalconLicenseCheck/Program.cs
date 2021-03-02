6923f4ed3066ffc1630eb7d7e6e574674392daaf
Bob Voigt <14822173+rtigithub@users.noreply.github.com>
2021-03-02 09:15:46 -0500

    Output status of HALCON environment variables.

--- HalconLicenseCheck/Program.cs
+++ HalconLicenseCheck/Program.cs
@@ -12,7 +12,14 @@
                {
                     var halconRoot = Environment.GetEnvironmentVariable("HALCONROOT");
                     var halconArch = Environment.GetEnvironmentVariable("HALCONARCH");
-                    Console.WriteLine($@"HALCON is installled in {halconRoot}\bin\{halconArch}.");
+                    if (string.IsNullOrEmpty(halconRoot) || string.IsNullOrEmpty(halconArch))
+                    {
+                         Console.WriteLine("HALCONROOT or HALCONARCH environment variables are not set.");
+                    }
+                    else
+                    {
+                         Console.WriteLine($@"HALCONROOT = {halconRoot}; HALCONARCH = {halconArch}.");
+                    }                    
                     Console.WriteLine($@"HALCON version is {HSystem.GetSystemInfo("version")};{Environment.NewLine}" +
                          $@"available license files are {HSystem.GetSystemInfo("available_license_files")}'{Environment.NewLine}" +
                          $@"current license info is {HSystem.GetSystemInfo("current_license_info")};{Environment.NewLine}" +
@@ -20,7 +27,7 @@
                }
                catch (HOperatorException hEx)
                {
-                    Console.WriteLine($"HALCON unable to initialize. Please check if license and dongle are installed correctly.\r\n{hEx.Message}", "HALCON Exception");
+                    Console.WriteLine($"HALCON unable to initialize. Please check if license and dongle are installed correctly.{Environment.NewLine}{hEx.Message}", "HALCON Exception");
                     Environment.Exit(hEx.GetErrorCode());
                }
           }
